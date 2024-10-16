54577f1909edd33e438001ab14a23901aa66d75e
Reham <hasib.rci@gmail.com>
2024-10-16 15:10:20 +0600

    2nd commit

--- js/rectanglecalculate.js
+++ js/rectanglecalculate.js
@@ -1,21 +1,22 @@
 function calculateRectangleArea(){
-    // get base input
+    // get Width input
     const rectangleWidhtInput = document.getElementById('rectangle-width');
     const rectangleWidthText = rectangleWidhtInput.value ;
     const width = parseFloat(rectangleWidthText);
-    console.log(width);
-    // get height input
-    /*const triangleHeightInput = document.getElementById('triangle-height');
-    const triangleHeightText = triangleHeightInput.value ;
-    const height = parseFloat(triangleHeightText);
-    // console.log(height);
+    // console.log(width);
+    
+    // get Length input
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value ;
+    const length = parseFloat(rectangleLengthText);
+    // console.log(length);
 
-    // calculate triangle area
-    const triangleArea = 0.5 * base * height;
-    // console.log(triangleArea);
+     // calculate triangle area
+    const rectangleArea = width * length;
+    // console.log(rectangleArea);
 
     // display triangle area
-    const triangleAreaSpan = document.getElementById('triangle-area');
-    triangleAreaSpan.innerHTML = triangleArea; */
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerHTML = rectangleArea; 
 
 }
